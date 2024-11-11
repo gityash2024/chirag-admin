142bf2659331acfbd9ec9064d073fcd53d1976e3
yash jangid <yash@ith.tech>
2024-11-11 18:12:11 +0530

    production codebase 2

--- src/environment/environment.tsx
+++ src/environment/environment.tsx
@@ -1,5 +1,5 @@
-const baseUrl = "http://localhost:5000/api";
-// const baseUrl = "https://chirag-backend.onrender.com/api";
+// const baseUrl = "http://localhost:5000/api";
+const baseUrl = "https://chirag-backend.onrender.com/api";
 // const baseUrl = "http://loca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lhost:5000/api";
 // const baseUrl = "https://chirag.solminica.com/api";
 
