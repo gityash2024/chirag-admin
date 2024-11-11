8efe92b9b813a1518cc38c855b7513d3d9a09282
yash jangid <yash@ith.tech>
2024-11-11 18:38:53 +0530

    code merge

--- src/environment/environment.tsx
+++ src/environment/environment.tsx
@@ -2,5 +2,4 @@
 const baseUrl = "https://chirag-backend.onrender.com/api";
 // const baseUrl = "http://loca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lhost:5000/api";
 // const baseUrl = "https://chirag.solminica.com/api";
-
 export { baseUrl };
